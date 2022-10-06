1d9f293c9608ea6f473c8a106288aa89d2c14192
sonodima <62671493+sonodima@users.noreply.github.com>
2022-10-06 12:59:59 +0200

    better error checking

--- main.go
+++ main.go
@@ -16,14 +16,19 @@ var config Config
 // Generated list of domains regular expressions to block
 var blacklist []string
 
+// Sends an error response to the client.
+func sendError(w dns.ResponseWriter, r *dns.Msg) error {
+	msg := &dns.Msg{}
+	msg.SetReply(r)
+	msg.Rcode = dns.RcodeNameError
+	return w.WriteMsg(msg)
+}
+
 // Function that handles DNS requests, and selectively blocks them or forwards them to the upstream DNS server.
 func handler(w dns.ResponseWriter, r *dns.Msg) {
 	if len(r.Question) == 0 {
 		log.Println("Received empty DNS request")
-		msg := &dns.Msg{}
-		msg.SetReply(r)
-		msg.Rcode = dns.RcodeNameError
-		w.WriteMsg(msg)
+		sendError(w, r)
 		return
 	}
 
@@ -43,16 +48,9 @@ func handler(w dns.ResponseWriter, r *dns.Msg) {
 				log.Println(color.RedString("[BLOCKED]"), domain)
 			}
 
-			msg := &dns.Msg{}
-			msg.SetReply(r)
-			msg.Rcode = dns.RcodeNameError
-
-			err = w.WriteMsg(msg)
-			if err != nil {
-				log.Println("Error writing response:", err)
-			} else {
-				return
-			}
+			// Send an error response
+			sendError(w, r)
+			return
 		}
 	}
 
@@ -61,17 +59,21 @@ func handler(w dns.ResponseWriter, r *dns.Msg) {
 		log.Println(color.GreenString("[ALLOWED]"), domain)
 	}
 
+	// Forward the request to the upstream DNS server
 	c := &dns.Client{}
 	msg, _, err := c.Exchange(r, "8.8.8.8:53")
 	if err != nil {
 		log.Println("Error resolving request:", err)
+
+		// Send an error response
+		sendError(w, r)
+		return
 	}
 
+	// Send the response to the client
 	err = w.WriteMsg(msg)
 	if err != nil {
 		log.Println("Error writing response:", err)
-	} else {
-		return
 	}
 }
 
